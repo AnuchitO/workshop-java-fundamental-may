780edcd0116c43ea18d2f6ee5066901236879d52
AnuchitO <3372362+AnuchitO@users.noreply.github.com>
2025-05-19 14:01:27 +0700

    type

---
+++ 01-starting-point/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+            float interestRate = 7.5f; // 5% interest rate
+            int principal = 1000; // Principal amount
+            double interest = principal * (interestRate / 100); // Calculate interest
+
+            char grade = 'A'; // Grade variable
+            boolean isPassed = true; // Boolean variable
+            String name = "John Doe"; // String variable
+            byte age = 30; // Age variable
+
+    
+        System.out.println("Interest Rate: " + interestRate + "%");
+    }
+}
