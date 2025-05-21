e2827af3ac70c33f81ab017cc5968b5a891d4815
AnuchitO <3372362+AnuchitO@users.noreply.github.com>
2025-05-21 16:24:13 +0700

    demo springboot

---
+++ demo-springboot/src/main/java/dev/anuchito/demo/LoanController.java
@@ -0,0 +1,29 @@
+package dev.anuchito.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoanController {
+
+	// URI compoents
+	// scheme://host:port/path/{id}?query#fragment
+	// http://localhost:8080/loans/234?name=anuchito&amount=10000
+
+ 
+ 	@GetMapping("/loans/{id}")
+	public Loan getLoan(@PathVariable("id") String id) {
+		System.out.println("Path id: " + id);
+
+		Loan loan = new Loan();
+		loan.setLoanId("L001");
+		loan.setApplicantName("Laumcing");
+		loan.setLoanAmount(1000000);
+		loan.setLoanTerm(12);
+		loan.setStatus("Approved");
+		loan.setInterestRate(7.5);
+
+		return loan;
+	}
+}
