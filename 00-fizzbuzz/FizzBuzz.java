c75e4261794e14e8ec5e77d5e42aa3518286ce56
AnuchitO <3372362+AnuchitO@users.noreply.github.com>
2025-05-21 14:35:13 +0700

    fizzbuzz

---
+++ 00-fizzbuzz/FizzBuzz.java
@@ -0,0 +1,11 @@
+
+import java.util.List;
+
+public class FizzBuzz {
+	public static void main(String[] args) {
+		List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 30);
+        // sol1 : for loop
+
+        // sol2 : use class and method
+	}
+}
