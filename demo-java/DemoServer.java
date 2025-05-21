b5399fa3bcd4ebc512be277c16ce82eba099a8c1
AnuchitO <3372362+AnuchitO@users.noreply.github.com>
2025-05-21 15:39:52 +0700

    Demo Java Server

---
+++ demo-java/DemoServer.java
@@ -0,0 +1,14 @@
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+
+
+public class DemoServer {
+	public static void main(String[] args) throws Exception {
+		HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+		server.createContext("/loans", new LoanHandler());
+		server.setExecutor(null);
+		System.out.println("Starting server on port 8080");
+		server.start();
+	}
+}
